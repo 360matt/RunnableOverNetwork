25966234e8680ca8c200ee1d123344b4fab11ef8
360matt <mattbacq2004@gmail.com>
2021-07-06 16:50:27 +0200

    Remove bytes logging

--- src/main/java/fr/i360matt/runnableOverNetwork/DynamicServer.java
+++ src/main/java/fr/i360matt/runnableOverNetwork/DynamicServer.java
@@ -58,7 +58,7 @@ public class DynamicServer implements Closeable {
                     while((read = receiver.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
                         totalRead += read;
                         remaining -= read;
-                        System.out.println("read " + totalRead + " bytes.");
+                       // System.out.println("read " + totalRead + " bytes.");
                         fos.write(buffer, 0, read);
                     }
                     fos.close();
